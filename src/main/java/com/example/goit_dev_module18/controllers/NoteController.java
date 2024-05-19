d5778689a240637a37c994d277655675b90c51d3
pavlo_hertsun <pavlo.hertsun.pz.2021@lpnu.ua>
2024-05-19 22:07:09 +0300

    added comments in note controller

--- src/main/java/com/example/goit_dev_module18/controllers/NoteController.java
+++ src/main/java/com/example/goit_dev_module18/controllers/NoteController.java
@@ -4,9 +4,7 @@ import com.example.goit_dev_module18.dtos.NoteDto;
 import com.example.goit_dev_module18.exceptions.NotValidNoteException;
 import com.example.goit_dev_module18.exceptions.NoteNotExistException;
 import com.example.goit_dev_module18.services.NoteService;
-import com.example.goit_dev_module18.utils.NoteMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,18 +20,22 @@ public class NoteController {
         this.noteService = noteService;
     }
 
+    // method returns the list of notes
     @GetMapping("")
     public ResponseEntity<List<NoteDto>> getAllNotes(){
         return ResponseEntity
                 .ok(noteService.getAllNotes());
     }
 
+    // method returns the note by its id
     @GetMapping("/{id}")
     public ResponseEntity<NoteDto> getNoteById(@PathVariable long id) throws NoteNotExistException {
         return ResponseEntity
                 .ok(noteService.getNoteById(id));
     }
 
+    // method creates new note
+    // params: NoteDto object with fields : title, content
     @PostMapping("")
     public ResponseEntity<NoteDto> createNote(@RequestBody NoteDto noteDto) throws NotValidNoteException {
         NoteDto createdNote = noteService.createNote(noteDto);
@@ -41,7 +43,8 @@ public class NoteController {
                 .status(201)
                 .body(createdNote);
     }
-
+    // method updates existing note
+    // params: NoteDto object with fields : id, title, content
     @PutMapping("")
     public ResponseEntity<NoteDto> updateNote(@RequestBody NoteDto noteDto)
             throws NoteNotExistException, NotValidNoteException{
@@ -49,6 +52,7 @@ public class NoteController {
                 .ok(noteService.updateNote(noteDto));
     }
 
+    // method delete existing note by its id
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteNote(@PathVariable long id) throws NoteNotExistException{
         noteService.deleteNote(id);
